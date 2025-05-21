5ad1f5a97b616895c28a3ee6c569f8b03c0c1d74
Edvaldo Junior <edonejunior@hotmail.com>
2025-05-20 21:05:17 -0300

    API - Consumo Nome - ID - Image

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const pokemonName = document.querySelector('.pokemon_name');
+const pokemonNumber = document.querySelector('.pokemon_number');
+const pokemonImage = document.querySelector('.pokemon_image');
 
 const fetchPokemon = async (pokemon) => {
 const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
@@ -9,6 +11,8 @@ return data;
 const renderPokemon = async (pokemon) => {
     const data = await fetchPokemon(pokemon);
     pokemonName.innerHTML = data.name;
+    pokemonNumber.innerHTML = data.id;
+    pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
 }
 
-renderPokemon('1')
+renderPokemon('649');
