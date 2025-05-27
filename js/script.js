062afe334509542db2ad06d8226c2a7a47c36aaa
Edvaldo Junior <edonejunior@hotmail.com>
2025-05-27 20:14:02 -0300

    Add Form

--- js/script.js
+++ js/script.js
@@ -2,6 +2,9 @@ const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
 
+const form = document.querySelector('.form');
+const input = document.querySelector('.input_search');
+
 const fetchPokemon = async (pokemon) => {
 const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
 const data = await apiResponse.json();
@@ -15,4 +18,9 @@ const renderPokemon = async (pokemon) => {
     pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
 }
 
-renderPokemon('649');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    renderPokemon(input.value.toLowerCase());
+    input.value = '';
+});
+
