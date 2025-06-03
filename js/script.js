9d9cfbc5c6548544865dc39fc8ed265bef8d5c87
Edvaldo Junior <edonejunior@hotmail.com>
2025-06-03 20:09:55 -0300

    Ajuste botões

--- js/script.js
+++ js/script.js
@@ -5,17 +5,39 @@ const pokemonImage = document.querySelector('.pokemon_image');
 const form = document.querySelector('.form');
 const input = document.querySelector('.input_search');
 
+const buttonPrev = document.querySelector('.btn-prev');
+const buttonNext = document.querySelector('.btn-next');
+
+let searchPokemon = '1'; // Default search
+
 const fetchPokemon = async (pokemon) => {
 const apiResponse = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-const data = await apiResponse.json();
-return data;
+
+    if(apiResponse.status === 200){
+        const data = await apiResponse.json();
+        return data;
+    }
+
 }
 
 const renderPokemon = async (pokemon) => {
+    pokemonName.innerHTML = 'Loading...';
+    pokemonNumber.innerHTML = '';
+    pokemonImage.src = '';
+
     const data = await fetchPokemon(pokemon);
-    pokemonName.innerHTML = data.name;
-    pokemonNumber.innerHTML = data.id;
-    pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+    if(data) {
+        pokemonName.innerHTML = data.name;
+        pokemonNumber.innerHTML = data.id;
+        pokemonImage.style.display = 'block';
+        pokemonImage.src = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+        searchPokemon = data.id; // Update searchPokemon to the current pokemon id
+    } else {
+        pokemonName.innerHTML = 'Not found :(';
+        pokemonNumber.innerHTML = '';
+        pokemonImage.style.display = 'none';
+    }
+   
 }
 
 form.addEventListener('submit', (event) => {
@@ -23,4 +45,17 @@ form.addEventListener('submit', (event) => {
     renderPokemon(input.value.toLowerCase());
     input.value = '';
 });
+
+buttonPrev.addEventListener('click', () => {
+    if (searchPokemon > 1) { // Prevent going below 1
+        searchPokemon -= 1; // Decrement searchPokemon
+        renderPokemon(searchPokemon);
+    }
+});
+buttonNext.addEventListener('click', () => {
+    searchPokemon += 1; // Increment searchPokemon
+    renderPokemon(searchPokemon);
+});
+
+renderPokemon(searchPokemon); // Initial render
 
